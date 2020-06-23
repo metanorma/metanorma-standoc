95dff09ed3fc9e89e886e0c91195522e10e1112f
Nick Nicholas <opoudjis@gmail.com>
2020-06-23 19:48:15 +1000

    presentation XML refactor: metanorma/metanorma-standoc#285

--- lib/metanorma/standoc/processor.rb
+++ lib/metanorma/standoc/processor.rb
@@ -25,12 +25,14 @@ module Metanorma
         Metanorma::Input::Asciidoc.new.process(file, filename, @asciidoctor_backend)
       end
 
-      def output(isodoc_node, outname, format, options={})
+      def output(isodoc_node, inname, outname, format, options={})
         case format
         when :html
-          IsoDoc::HtmlConvert.new(options).convert(outname, isodoc_node)
+          IsoDoc::HtmlConvert.new(options).convert(inname, isodoc_node, nil, outname)
         when :doc
-          IsoDoc::WordConvert.new(options).convert(outname, isodoc_node)
+          IsoDoc::WordConvert.new(options).convert(inname, isodoc_node, nil, outname)
+        when :presentation
+          IsoDoc::PresentationXMLConvert.new(options).convert(inname, isodoc_node, nil, outname)
         else
           super
         end
