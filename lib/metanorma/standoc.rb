d2799aab3cc91cafa00a7b0c9d3583f461107528
Alexande B <abobrikovich@gmail.com>
2020-09-18 14:28:43 +0300

    #332 remove LateXML requirments class

--- lib/metanorma/standoc.rb
+++ lib/metanorma/standoc.rb
@@ -1,11 +1,6 @@
 require_relative "./standoc/processor"
-require_relative "./standoc/requirement"
-require_relative "./standoc/latexml_requirement"
 
 module Metanorma
   module Standoc
-    Requirements = {
-      latexml: LatexmlRequirement.new
-    }
   end
 end
