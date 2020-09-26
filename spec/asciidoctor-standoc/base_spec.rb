35f5c92128a659f1025f11c922d7228c80e0f07b
Nick Nicholas <opoudjis@gmail.com>
2020-09-26 18:46:14 +1000

    rspec

--- spec/asciidoctor-standoc/base_spec.rb
+++ spec/asciidoctor-standoc/base_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe Asciidoctor::Standoc do
       :novalid:
       :publisher: International Electrotechnical Commission,IETF,ISO
 INPUT
-<standard-document xmlns='https://www.metanorma.org/ns/standoc'>
+<standard-document xmlns='https://www.metanorma.org/ns/standoc' type='semantic' version='1.5.3'>
   <bibdata type='standard'>
     <title language='en' format='text/plain'>Document title</title>
     <contributor>
