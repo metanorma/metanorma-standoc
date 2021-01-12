f5849118b182ae2cb01a5f01f4e99b35fc5efcc1
Nick Nicholas <opoudjis@gmail.com>
2021-01-12 23:02:54 +1100

    custom clause type: https://github.com/metanorma/metanorma/issues/122

--- lib/asciidoctor/standoc/section.rb
+++ lib/asciidoctor/standoc/section.rb
@@ -1,6 +1,6 @@
-require "htmlentities"
 require "uri"
 require_relative "ref_sect"
+require_relative "terms"
 
 module Asciidoctor
   module Standoc
@@ -8,10 +8,6 @@ module Asciidoctor
       @biblio = false
       @term_def = false
       @norm_ref = false
-
-      def in_terms?
-        @term_def
-      end
 
       def sectiontype1(node)
         node&.attr("heading")&.downcase || node.title.gsub(/<[^>]+>/, "").downcase
@@ -49,6 +45,7 @@ module Asciidoctor
                 language: node.attributes["language"],
                 script: node.attributes["script"],
                 number: node.attributes["number"],
+                type: node.attributes["type"],
                 annex: ( ((node.attr("style") == "appendix" || node.role == "appendix") && 
                           node.level == 1) ? true : nil),
                          preface: (
@@ -144,80 +141,6 @@ module Asciidoctor
         xml.annex **attr_code(attrs) do |xml_section|
           xml_section.title { |name| name << node.title }
           xml_section << node.content
-        end
-      end
-
-      def nonterm_symbols_parse(attrs, xml, node)
-        defs = @definitions
-        @definitions = false
-        clause_parse(attrs, xml, node)
-        @definitions = defs
-      end
-
-      def symbols_attrs(node, a)
-        case sectiontype1(node)
-        when "symbols" then a.merge(type: "symbols")
-        when "abbreviated terms", "abbreviations" then a.merge(type: "abbreviated_terms")
-        else
-          a
-        end
-      end
-
-      def symbols_parse(attr, xml, node)
-        node.role == "nonterm" and return nonterm_symbols_parse(attr, xml, node)
-        xml.definitions **attr_code(attr) do |xml_section|
-          xml_section.title { |t| t << node.title }
-          defs = @definitions
-          termdefs = @term_def
-          @definitions = true
-          @term_def = false
-          xml_section << node.content
-          @definitions = defs
-          @term_def = termdefs
-        end
-      end
-
-      def nonterm_term_def_subclause_parse(attrs, xml, node)
-        defs = @term_def
-        @term_def = false
-        clause_parse(attrs, xml, node)
-        @term_def = defs
-      end
-
-      def terms_boilerplate_parse(attrs, xml, node)
-        defs = @term_def
-        @term_def = false
-        clause_parse(attrs.merge(type: "boilerplate"), xml, node)
-        @term_def = defs
-      end
-
-      # subclause contains subclauses
-      def term_def_subclause_parse(attrs, xml, node)
-        node.role == "nonterm"  and return nonterm_term_def_subclause_parse(attrs, xml, node)
-        node.role == "boilerplate"  and return terms_boilerplate_parse(attrs, xml, node)
-        st = sectiontype(node, false)
-        return symbols_parse(attrs, xml, node) if @definitions
-        sub = node.find_by(context: :section) { |s| s.level == node.level + 1 }
-        sub.empty? || (return term_def_parse(attrs, xml, node, false))
-        st == "symbols and abbreviated terms" and (return symbols_parse(attrs, xml, node))
-        st == "terms and definitions" and return clause_parse(attrs, xml, node)
-        term_def_subclause_parse1(attrs, xml, node)
-      end
-
-      def term_def_subclause_parse1(attrs, xml, node)
-        xml.term **attr_code(attrs) do |xml_section|
-          xml_section.preferred { |name| name << node.title }
-          xml_section << node.content
-        end
-      end
-
-      def term_def_parse(attrs, xml, node, toplevel)
-        xml.terms **attr_code(attrs) do |section|
-          section.title { |t| t << node.title }
-          (s = node.attr("source")) && s.split(/,/).each do |s1|
-            section.termdocsource(nil, **attr_code(bibitemid: s1))
-          end
-          section << node.content
         end
       end
 
