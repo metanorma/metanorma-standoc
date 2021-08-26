c8fcd600c6adf33638cead1373b61115dd1a23f4
MikhailTretyakov <8926238@gmail.com>
2021-08-26 13:57:34 +0300

    Use different macro class - lutaml_table

--- lib/asciidoctor/standoc/converter.rb
+++ lib/asciidoctor/standoc/converter.rb
@@ -50,10 +50,7 @@ module Asciidoctor
         inline_macro Asciidoctor::Standoc::FormOptionMacro
         inline_macro Asciidoctor::Standoc::ToCInlineMacro
         inline_macro Metanorma::Plugin::Lutaml::LutamlFigureInlineMacro
-        inline_macro Metanorma::Plugin::Lutaml::LutamlTablePackageInlineMacro
-        inline_macro Metanorma::Plugin::Lutaml::LutamlTableDataTypeInlineMacro
-        inline_macro Metanorma::Plugin::Lutaml::LutamlTableEnumInlineMacro
-        inline_macro Metanorma::Plugin::Lutaml::LutamlTableClassInlineMacro
+        inline_macro Metanorma::Plugin::Lutaml::LutamlTableInlineMacro
         block_macro Metanorma::Plugin::Lutaml::LutamlDiagramBlockMacro
         block Asciidoctor::Standoc::ToDoAdmonitionBlock
         treeprocessor Asciidoctor::Standoc::ToDoInlineAdmonitionBlock
