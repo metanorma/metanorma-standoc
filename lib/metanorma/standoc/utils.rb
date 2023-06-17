3dcce65690317ebc22d3dc750d7265bdd1128aba
Nick Nicholas <opoudjis@gmail.com>
2023-06-18 02:37:07 +0300

    https://github.com/metanorma/metanorma-standoc/issues/233

--- lib/metanorma/standoc/utils.rb
+++ lib/metanorma/standoc/utils.rb
@@ -76,12 +76,11 @@ module Metanorma
           .gsub(/&apos;/, "'")
       end
 
-
       # wrapped in <sections>
       def adoc2xml(text, flavour)
         Nokogiri::XML(text).root and return text
         c = Asciidoctor.convert("= X\nA\n:semantic-metadata-headless: true\n" \
-                                "\n#{text}\n",
+                                ":novalid:\n\n#{text}\n",
                                 backend: flavour, header_footer: true)
         Nokogiri::XML(c).at("//xmlns:sections")
       end
