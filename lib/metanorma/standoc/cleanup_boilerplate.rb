5c1b664f8395d4191da4d8289949395ced6a389e
Nick Nicholas <opoudjis@gmail.com>
2024-02-11 17:16:15 +1100

    term boilerplate processing: differentiate %1 (for injecting other text) from % (reserved  for injecting sources):https://github.com/metanorma/metanorma-bsi/issues/476

--- lib/metanorma/standoc/cleanup_boilerplate.rb
+++ lib/metanorma/standoc/cleanup_boilerplate.rb
@@ -3,14 +3,16 @@ module Metanorma
     module Cleanup
       def external_terms_boilerplate(sources)
         @i18n.l10n(
-          @i18n.external_terms_boilerplate.gsub(/%\b/, sources || "???"),
+          @i18n.external_terms_boilerplate.gsub(/%(?=\p{P}|\p{Z}|$)/,
+                                                sources || "???"),
           @lang, @script, @locale
         )
       end
 
       def internal_external_terms_boilerplate(sources)
         @i18n.l10n(
-          @i18n.internal_external_terms_boilerplate.gsub(/%\b/, sources || "??"),
+          @i18n.internal_external_terms_boilerplate.gsub(/%(?=\p{P}|\p{Z}|$)/,
+                                                         sources || "??"),
           @lang, @script
         )
       end
