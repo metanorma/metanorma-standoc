8db23e5a4a93d09db0136bf5b74727637382ad78
Nick Nicholas <opoudjis@gmail.com>
2024-08-30 01:15:46 +1000

    use Asciidoc for termdef boilerplate: https://github.com/metanorma/metanorma-iec/issues/339

--- lib/metanorma/standoc/cleanup.rb
+++ lib/metanorma/standoc/cleanup.rb
@@ -55,6 +55,10 @@ module Metanorma
         biblio_cleanup(xmldoc)
         reference_names(xmldoc)
         terms_terms_cleanup(xmldoc) # feeds: boilerplate_cleanup
+        docidentifier_cleanup(xmldoc) # feeds: bibdata_cleanup
+        ext_contributor_cleanup(xmldoc) # feeds: bibdata_cleanup
+        ext_dochistory_cleanup(xmldoc) # feeds: bibdata_cleanup
+        bibdata_cleanup(xmldoc) # feeds: boilerplate_cleanup
         boilerplate_cleanup(xmldoc) # feeds: xref_cleanup
         asciimath_cleanup(xmldoc) # feeds: mathml_cleanup, termdef_cleanup,
         # symbols_cleanup
@@ -76,10 +80,6 @@ module Metanorma
         ol_cleanup(xmldoc)
         mathml_cleanup(xmldoc)
         script_cleanup(xmldoc)
-        docidentifier_cleanup(xmldoc) # feeds: bibdata_cleanup
-        ext_contributor_cleanup(xmldoc) # feeds: bibdata_cleanup
-        ext_dochistory_cleanup(xmldoc) # feeds: bibdata_cleanup
-        bibdata_cleanup(xmldoc)
         svgmap_cleanup(xmldoc) # feeds: img_cleanup
         toc_cleanup(xmldoc)
         smartquotes_cleanup(xmldoc)
