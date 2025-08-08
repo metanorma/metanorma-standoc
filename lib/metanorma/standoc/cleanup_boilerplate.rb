e9f071fa2d1d9ed57953a05a32d171f087a251ba
Ronald Tse <ronald.tse@ribose.com>
2025-08-08 18:30:32 +0800

    chore: describe why boilerplate cleanup is needed and how it works

--- lib/metanorma/standoc/cleanup_boilerplate.rb
+++ lib/metanorma/standoc/cleanup_boilerplate.rb
@@ -162,16 +162,49 @@ module Metanorma
         end
       end
 
-      # Asciidoc macro, e.g. span:publisher[...]
-      # May contain one or more {{ }} in target, with spaces in them
-      # Does not end in \]
+      # The boilerplate file is in Liquid AsciiDoc format
+      # (technically, `boilerplate.adoc.liquid`).
+      #
+      # This file is processed separately from the main Metanorma document and
+      # therefore is oblivious of the `{{ concept-mention }}` syntax.
+      #
+      # Due to historic reasons, the Liquid objects being evaluated in the
+      # boilerplate document are XML strings. Notably these are the document
+      # metadata, that are extracted from the already generated Metanorma XML.
+      #
+      # These XML strings are then passed into the AsciiDoc macros such as
+      # `span:publisher[...]`.
+      #
+      # Here, we need to interpolate the XML strings into the AsciiDoc macros
+      # without breaking the AsciiDoc syntax.
+      #
+      # EXAMPLE 1: `mailto:{{ pub_email }}[]`, we need to
+      # convert it to:
+      # `mailto:{{ pass-format:metanorma[++pub_email_xml++] }}[]`
+      #
+      # EXAMPLE 2: `link:{{ pub_uri}}[{{ pub_address }}, {{ pub_uri }}]`
+      # We need to convert it to:
+      # `link:{{ pass-format:metanorma[++pub_uri_xml++] }}[{{
+      # pass-format:metanorma[++pub_address_xml++] }}, {{
+      # pass-format:metanorma[++pub_uri_xml++] }}]`
+      #
+      # NOTE: The boilerplate may use macros that contain one or more
+      # `{{ ... }}` in the target, and can contain spaces in them.
+      #
+      # NOTE: The routine needs to handle cases where the content
+      # contains an escaped closing bracket `\]`.
+
       ADOC_MACRO_START =
         '\S+:(?:[^\[\] ]+|\{\{[^{}]+\}\}){0-999}\[.*?(?<!\\\\)\]'.freeze
+      # Replace {{ ... }} with {{ pass-format:metanorma:[...] }} to preserve any
+      # XML markup provided by Metanorma XML Metadata content, through the
+      # `pass-format:metanorma` command.
+      #
+      # * If `{{ ... }}` is inside an Asciidoc macro, we have to wrap with
+      #   pass-format:metanorma:[...\].
+      # * If this is a macro target (e.g. `mailto:{{x}}[]`, body: mailto:[{{x}}])
+      #   then do not use pass-format:metanorma.
 
-      # Replace {{ ... }} with {{ pass:[...]}} to preserve any XML markup
-      # use pass:[...\] if {{}} is already inside an Asciidoc macro
-      # Do not use pass: if this is a macro target: mailto:{{x}}[]
-      # or body: mailto:[{{x}}]
       def boilerplate_read(file)
         ret = File.read(file, encoding: "UTF-8")
         /\.adoc$/.match?(file) or return ret
