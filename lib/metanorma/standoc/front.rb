8159043d90338dd8c22f0ae14cdf0d53328f267b
Nick Nicholas <opoudjis@gmail.com>
2025-12-18 22:49:05 +0800

    save i18n'd title prefixes to numeric title components in Semantic XML: https://github.com/metanorma/metanorma-plateau/issues/299

--- lib/metanorma/standoc/front.rb
+++ lib/metanorma/standoc/front.rb
@@ -3,6 +3,7 @@ require "pathname"
 require_relative "./front_contributor"
 require_relative "./front_ext"
 require "isoics"
+require "isodoc"
 
 module Metanorma
   module Standoc
@@ -184,6 +185,7 @@ module Metanorma
         title_main(node, xml)
         title_other(node, xml)
         title_fallback(node, xml)
+        title_nums(node, xml, @lang)
       end
 
       # English plain title: :title: or implicit, typed as main
@@ -214,6 +216,31 @@ module Metanorma
       def title_fallback(node, xml)
         xml.parent.at("./title[not(normalize-space(.)='')]") and return
         add_title_xml(xml, node.attr("doctitle"), @lang, "main")
+      end
+
+      def title_nums(node, xml, lang)
+        @i18n_cache ||= {}
+        @i18n_cache[lang] ||= ::IsoDoc::I18n.new(lang, ::Metanorma::Utils::default_script(lang))
+        ret = title_nums_prep(node)
+        ret[:part] && ret[:subpart] and ret[:part] += "&#x2013;#{ret[:subpart]}"
+        ret.delete(:subpart)
+        ret.each do |k, v|
+          title_num_prefix(k, v, xml, lang)
+        end
+      end
+
+      def title_nums_prep(node)
+        part, subpart = node.attr("partnumber")&.split("-")
+        { part:, subpart:, amendment: node.attr("amendment-number"),
+          corrigendum: node.attr("corrigendum-number"),
+          addendum: node.attr("addendum-number") }
+      end
+
+      def title_num_prefix(key, value, xml, lang)
+        prefix = @i18n_cache[lang].get.dig("title_prefixes", key.to_s) or return
+        value && !value.empty? or return
+        title = "#{prefix}&#xa0;#{value}"
+        add_title_xml(xml, title, lang, "title-#{key}-prefix")
       end
     end
   end
